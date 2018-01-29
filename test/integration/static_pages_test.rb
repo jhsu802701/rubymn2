ab0257987c1392474e8d4bfb2191bb67d937888c
Jason Hsu <jhsu802701@jasonhsu.com>
2018-01-29 06:21:40 +0000

    Removed all content from the About and Contact pages

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -9,22 +9,6 @@ class StaticPagesTest < ActionDispatch::IntegrationTest
     assert page.has_css?('small', text: 'Ruby Users of Minnesota by Somebody')
   end
 
-  test 'about page has expected content' do
-    visit about_path
-    assert page.has_css?('title', text: full_title('About'), visible: false)
-    assert page.has_css?('h1', text: 'About')
-    assert page.has_text?('Describe your site here.')
-    assert page.has_css?('small', text: 'Ruby Users of Minnesota by Somebody')
-  end
-
-  test 'contact page has expected content' do
-    visit contact_path
-    assert page.has_css?('title', text: full_title('Contact'), visible: false)
-    assert page.has_css?('h1', text: 'Contact')
-    assert page.has_text?('rubyist@jasonhsu.com')
-    assert page.has_css?('small', text: 'Ruby Users of Minnesota by Somebody')
-  end
-
   test 'about page provides access to the home page' do
     visit about_path
     click_on 'Home'
