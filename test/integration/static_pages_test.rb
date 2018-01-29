2ce40a1fd1a34da78701c950a3f182c1b857c328
Jason Hsu <jhsu802701@jasonhsu.com>
2018-01-29 17:23:43 +0000

    Updated the text in the static pages

--- test/integration/static_pages_test.rb
+++ test/integration/static_pages_test.rb
@@ -6,6 +6,14 @@ class StaticPagesTest < ActionDispatch::IntegrationTest
     assert page.has_css?('title', text: full_title(''), visible: false)
     assert page.has_css?('h1', text: 'Home')
     assert page.has_text?('Welcome to Ruby Users of Minnesota')
-    assert page.has_css?('small', text: 'Ruby Users of Minnesota by Somebody')
+    assert page.has_css?('small', text: 'Ruby Users of Minnesota')
+
+    assert page.has_css?('small', text: Time.now.year.to_s)
+    assert page.has_link?('Heroku', href: 'https://www.heroku.com')
+    assert page.has_text?('We are Ruby enthusiasts from the Twin Cities area.')
+    assert page.has_link?('https://www.meetup.com/ruby-mn', href: 'https://www.meetup.com/ruby-mn')
+    assert page.has_link?('https://twitter.com/rubymn', href: 'https://twitter.com/rubymn')
+    assert page.has_link?('https://groups.google.com/forum/#!forum/rubymn', href: 'https://groups.google.com/forum/#!forum/rubymn')
+    assert page.has_link?('https://www.dayblockbrewing.com', href: 'https://www.dayblockbrewing.com')
   end
 end
