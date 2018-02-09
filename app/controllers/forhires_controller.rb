043f7ff66deeb4f094b8cb777588b737b4ea7b4d
Jason Hsu <jhsu802701@jasonhsu.com>
2018-02-09 18:19:12 +0000

    Added the forhire index capability (controller level)

--- app/controllers/forhires_controller.rb
+++ app/controllers/forhires_controller.rb
@@ -5,5 +5,10 @@ class ForhiresController < ApplicationController
     @forhire = Forhire.find(params[:id])
     @user = User.where("id=#{@forhire.user_id}").first
   end
+
+  def index
+    @forhires = Forhire.order('updated_at DESC').page(params[:page]).per(50)
+    @forhires_count = Forhire.count
+  end
   # END: action section
 end
