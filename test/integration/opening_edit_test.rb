9dea283401e700caf99e1c6a59ad31f903f912b5
Jason Hsu <jhsu802701@jasonhsu.com>
2018-02-26 20:58:26 +0000

    Added opening edit (view level)

---
+++ test/integration/opening_edit_test.rb
@@ -0,0 +1,50 @@
+require 'test_helper'
+
+class OpeningEditTest < ActionDispatch::IntegrationTest
+  def check_no_edit_button
+    visit opening_path(@op4)
+    assert page.has_no_link?('Edit Job Opening', href: opening_path(@op4))
+  end
+
+  test 'visitor may not edit opening' do
+    check_no_edit_button
+  end
+
+  test 'wrong user may not edit opening' do
+    login_as(@u2, scope: :user)
+    check_no_edit_button
+  end
+
+  test 'correct user gets button to edit opening' do
+    login_as(@u11, scope: :user)
+    visit opening_path(@op4)
+    assert page.has_link?('Edit Job Opening', href: edit_opening_path(@op4))
+  end
+
+  test 'regular admin may not edit opening' do
+    login_as(@a4, scope: :admin)
+    check_no_edit_button
+  end
+
+  test 'super admin may not edit opening' do
+    login_as(@a1, scope: :admin)
+    check_no_edit_button
+  end
+
+  test 'correct user can successfully edit opening' do
+    login_as(@u11, scope: :user)
+    visit edit_opening_path(@op4)
+
+    assert page.has_css?('title', text: full_title('Edit Job Opening'),
+                                  visible: false)
+    fill_in('Title', with: 'Lawyer')
+    fill_in('Description', with: 'Defend me from lawsuits')
+    click_button('Submit')
+
+    assert page.has_css?('title',
+                         text: full_title('Job Opening: Lawyer'),
+                         visible: false)
+    assert page.has_css?('h1', text: 'Job Opening: Lawyer')
+    assert_text 'Defend me from lawsuits'
+  end
+end
