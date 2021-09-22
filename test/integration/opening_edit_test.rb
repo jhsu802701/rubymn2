f645f38e8fd52c88ca3a742a244eee0d1719e012
Jason Hsu <jhsu802701@jasonhsu.com>
2021-09-22 15:12:32 -0500

    Added integration test of invalid edit of job opening

--- test/integration/opening_edit_test.rb
+++ test/integration/opening_edit_test.rb
@@ -31,10 +31,28 @@ class OpeningEditTest < ActionDispatch::IntegrationTest
     check_no_edit_button
   end
 
+  # rubocop:disable Metrics/BlockLength
   test 'correct user can successfully edit opening' do
     login_as(@u11, scope: :user)
-    visit edit_opening_path(@op4)
 
+    # Invalid edit of job opening
+    visit edit_opening_path(@op4)
+    assert page.has_css?('title', text: full_title('Edit Job Opening'),
+                                  visible: false)
+    fill_in('Title', with: '')
+    click_button('Submit')
+    assert page.has_text?('The form contains 1 error.')
+    assert page.has_text?("Title can't be blank")
+    visit opening_path(@op4)
+    assert page.has_css?('title',
+                         text: full_title('Job Opening: Deputy'),
+                         visible: false)
+    assert page.has_css?('h1', text: 'Job Opening: Deputy')
+    assert_text 'Catch that black Trans Am!'
+    assert page.has_link?('Jackie Gleason', href: user_path(@u11))
+
+    # Valid edit of job opening
+    visit edit_opening_path(@op4)
     assert page.has_css?('title', text: full_title('Edit Job Opening'),
                                   visible: false)
     fill_in('Title', with: 'Lawyer')
@@ -47,4 +65,5 @@ class OpeningEditTest < ActionDispatch::IntegrationTest
     assert page.has_css?('h1', text: 'Job Opening: Lawyer')
     assert_text 'Defend me from lawsuits'
   end
+  # rubocop:enable Metrics/BlockLength
 end
